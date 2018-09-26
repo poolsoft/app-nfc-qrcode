42e29a0a5424f610da87369449a60e7063acb4fa
Italo Josep <italojosep@gmail.com>
2018-09-26 00:20:27 -0300

    Tela de timestamp

--- app/src/main/java/com/br/syncrename/Fragments/TimestampFragment.java
+++ app/src/main/java/com/br/syncrename/Fragments/TimestampFragment.java
@@ -5,12 +5,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.br.syncrename.R;
 
+import org.joda.time.DateTime;
+
+import java.util.Date;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class TimestampFragment extends Fragment {
+
+    @BindView(R.id.text_timestamp)
+    TextView timestamp;
+
+    @BindView(R.id.text_date)
+    TextView dateText;
+
 
     public static TimestampFragment newInstance() {
         TimestampFragment fragment = new TimestampFragment();
@@ -18,10 +31,21 @@ public class TimestampFragment extends Fragment {
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_nfc, container, false);
+        View view = inflater.inflate(R.layout.fragment_timestamp, container, false);
         ButterKnife.bind(this, view);
 
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        DateTime date = new DateTime();
+        dateText.setText("qqqqqq");
+        timestamp.setText("aaaaaa");
+
+
+
+    }
 }
