5d5f0722e1f9588fc378b8856bf65a0ccac6b75e
Italo Josep <italojosep@gmail.com>
2018-09-29 12:41:41 -0300

    Gravando e escrevendo ok

--- app/src/main/java/com/br/syncrename/Activities/SyncActivity.java
+++ app/src/main/java/com/br/syncrename/Activities/SyncActivity.java
@@ -1,15 +1,56 @@
 package com.br.syncrename.Activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.br.syncrename.Models.Arquivo;
+import com.br.syncrename.R;
+import com.br.syncrename.Utils.ArquivoTxt;
 import com.br.syncrename.Utils.PreferenceHandler;
 
 public class SyncActivity extends AppCompatActivity {
+
+    private static final int REQUEST = 2424;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         PreferenceHandler.init(this);
+
+        verificarPermissoes();
+    }
+
+    public void verificarPermissoes(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                ) {
+            String[] PERMS = {
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(this, PERMS, REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch(requestCode) {
+            case REQUEST:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+
+                    Toast.makeText(this, getResources().getString(R.string.file_name), Toast.LENGTH_SHORT).show();
+                }
+                return;
+        }
     }
 }
