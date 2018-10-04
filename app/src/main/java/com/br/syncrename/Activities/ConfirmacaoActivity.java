2ffb2df7c3eb418b69736e5bb39c61e62dae8171
Italo Josep <italo@ivoryit.com.br>
2018-10-04 16:56:26 -0300

    gravando tag de usuario

--- app/src/main/java/com/br/syncrename/Activities/ConfirmacaoActivity.java
+++ app/src/main/java/com/br/syncrename/Activities/ConfirmacaoActivity.java
@@ -1,18 +1,30 @@
 package com.br.syncrename.Activities;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.br.syncrename.Models.Tags;
 import com.br.syncrename.R;
+import com.br.syncrename.Utils.ArquivoTxt;
 import com.br.syncrename.Utils.Constantes;
 import com.br.syncrename.Utils.PreferenceHandler;
+import com.br.syncrename.Utils.ServerHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ConfirmacaoActivity extends SyncActivity {
 
@@ -28,13 +40,13 @@ public class ConfirmacaoActivity extends SyncActivity {
     private String arquivoAtual;
     private String date1;
     private String millius;
+    private String code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmacao);
         ButterKnife.bind(this);
-
     }
 
     @Override
@@ -60,8 +72,55 @@ public class ConfirmacaoActivity extends SyncActivity {
         millius = String.valueOf(df.getCalendar().getTimeInMillis());
         dateText.setText(date1);
         timestamp.setText(millius);
+        separarQRCode();
         arquivoAtual = getIntent().getStringExtra(Constantes.ATUAL_TXT);
-        userCode.setText(getIntent().getStringExtra(Constantes.CODE_QRCODE));
+        userCode.setText(code);
     }
+
+    public void separarQRCode(){
+        code = getIntent().getStringExtra(Constantes.CODE_QRCODE);
+
+        try {
+            JSONObject jsonDefault = new JSONObject(code);
+            if(jsonDefault.has("qrcode")){
+                jsonDefault = jsonDefault.getJSONObject("qrcode");
+                if(jsonDefault.has("Id")){
+                    int codeint = jsonDefault.getInt("Id");
+                    code = String.valueOf(codeint);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        voltarMain();
+    }
+
+    @OnClick(R.id.button_nao) void voltarMain(){
+        Intent i = new Intent(this,MainActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        finish();
+        startActivity(i);
+    }
+
+    @OnClick(R.id.button_sim) void confirmaTag(){
+        Tags tags = new Tags();
+        tags.setCode(code);
+        tags.setTimeStamp(millius);
+        try {
+            String novoTag = ServerHandler.getJsonConverter().writeValueAsString(tags);
+            ArquivoTxt.gravarArquivo(this, novoTag, arquivoAtual);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 
 }
---
+++ app/src/main/java/com/br/syncrename/Models/Tags.java
@@ -0,0 +1,23 @@
+package com.br.syncrename.Models;
+
+public class Tags {
+
+    private String timeStamp;
+    private String Code;
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getCode() {
+        return Code;
+    }
+
+    public void setCode(String code) {
+        Code = code;
+    }
+}
